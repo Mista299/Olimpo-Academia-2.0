72bc06d2e9415d6c607868ea592cd55d48d4b06d
Mista299 <michael.tabares@udea.edu.co>
2025-02-08 16:33:53 -0500

    last changes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+// pages/Home.jsx
+import NavBar from '../components/Navbar'
+import Carousel from '../components/Carousel'
+import Footer from '../components/Footer'
+import MapSlider from '../components/MapSlider'
+
+function Home() {
+    return (
+      <>
+
+      </>
+
+    );
+  }
+
+  export default Home
---
+++ src/pages/Inscripciones.jsx
@@ -0,0 +1,10 @@
+// pages/Home.jsx
+function Inscripciones() {
+    return (
+      <div>
+        <h1>Bienvenido a la Página de Inicio</h1>
+      </div>
+    );
+  }
+
+export default Inscripciones
