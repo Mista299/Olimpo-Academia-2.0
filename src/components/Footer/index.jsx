df458fe1d402c202c31e8036cf04208e73276c1e
Mista299 <michael.tabares@udea.edu.co>
2025-02-14 18:05:09 -0500

    footer complete

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
+
 import SocialMedia from '../SocialMedia'
 import logo from '../../imgs/logo1.webp';
+import Modal from '../../components/Modal'
+
 import { NavLink } from "react-router-dom";
 
 
 const Footer = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
     return(
         <footer className="py-10 bg-[linear-gradient(90deg,#000428_0%,#004e92_100%)] sm:pt-16 lg:pt-24 relative w-full">
             <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
@@ -12,15 +20,15 @@ const Footer = () => {
                         <p className="text-base text-gray-500">Compañía</p>
                         <ul className="mt-8 space-y-4">
                             <li>
-                                <NavLink to="/Nosotros" className="text-base text-white hover:text-opacity-80">
+                                <NavLink to="/Nosotros" className=" text-base text-white hover:bg-[#61CE70]">
                                     Acerca de nosotros
                                 </NavLink>
                             </li>
                             <li>
-                                <a href="#" className="text-base text-white hover:text-opacity-80">Certificaciones</a>
+                                {/* <a href="#" className="text-base text-white hover:text-opacity-80">Certificaciones</a> */}
                             </li>
                             <li>
-                                <NavLink to="/Nosotros" className="text-base text-white hover:text-opacity-80">
+                                <NavLink to="/Nosotros" className="text-base text-white hover:bg-[#61CE70]">
                                     Trabaja con nosotros
                                 </NavLink>
                             </li>
@@ -33,10 +41,9 @@ const Footer = () => {
                                 <a href="https://api.whatsapp.com/send/?phone=573174535342&text=Hola+vengo+del+sitio+web%2C+y+necesito+m%C3%A1s+informaci%C3%B3n+sobre+las+membres%C3%ADas+y+servicios&type=phone_number&app_absent=0" className="text-base text-white hover:text-opacity-80">Soporte al usuario</a>
                             </li>
                             <li>
-                                <a href="#" className="text-base text-white hover:text-opacity-80">Términos y condiciones</a>
                             </li>
                             <li>
-                                <a href="/politicaPrivacidad" className="text-base text-white hover:text-opacity-80">Política de privacidad</a>
+                                <a className="text-base text-white hover:text-opacity-80" onClick={openModal}>Política de privacidad</a>
                             </li>
                         </ul>
                     </div>
@@ -45,10 +52,10 @@ const Footer = () => {
                         <ul className="mt-8 space-y-4">
 
                             <li>
-                                <a href="#" className="text-base text-white hover:text-opacity-80">Clases personalizadas</a>
+                                <a href="https://api.whatsapp.com/send/?phone=573174535342&text=Hola+vengo+del+sitio+web%2C+y+necesito+m%C3%A1s+informaci%C3%B3n+sobre+las+membres%C3%ADas+y+servicios&type=phone_number&app_absent=0" className="text-base text-white hover:text-opacity-80">Clases personalizadas</a>
                             </li>
                             <li>
-                                <a href="#" className="text-base text-white hover:text-opacity-80">Tutoriales de gimnasia</a>
+                                <a href="https://www.youtube.com/watch?v=x5vZIo0dJL8" className="text-base text-white hover:text-opacity-80">Tutoriales de gimnasia</a>
                             </li>
                         </ul>
                     </div>
@@ -71,6 +78,18 @@ const Footer = () => {
                     </div>
                     <SocialMedia/>
                 </div>
+                <Modal isOpen={isModalOpen} onClose={closeModal}>
+                    <div className="max-h-[50vh] overflow-y-auto p-4">
+                        <h2 className="text-2xl mb-4">Política de privacidad y de tratamiento de datos personales</h2>
+                        <p className="text-justify">
+                            En Club olimpo de gimnasia, reconocemos la importancia de la privacidad y la protección de los datos personales de nuestros usuarios. A pesar de no utilizar cookies, recopilamos información limitada y necesaria para mejorar la experiencia del usuario y brindar un servicio personalizado. Los datos proporcionados, como nombre, correo electrónico o información de contacto, se utilizan exclusivamente para facilitar la comunicación entre el sitio y sus usuarios, garantizando la confidencialidad y no compartiéndolos con terceros sin consentimiento expreso. Todos los datos se almacenan de forma segura y se eliminan cuando ya no sean necesarios para los fines establecidos. Nuestra prioridad es salvaguardar la información de nuestros usuarios y asegurar el cumplimiento de las normativas de protección de datos vigentes.
+                            <br />
+                            Al aceptar la casilla de verificación y utilizar el sitio web Club olimpo de gimnasia, usted acepta automáticamente los términos y condiciones de nuestra Política de Tratamiento de Datos Personales. Si no está de acuerdo con estos términos, le recomendamos abstenerse de utilizar nuestros servicios. Su uso continuo del sitio implica la aceptación de nuestra política de privacidad y el manejo de sus datos personales según lo establecido en la misma.
+                            <br />
+                            Club Olimpo Copyright 2024
+                        </p>
+                    </div>
+                </Modal>
             </div>
         </footer>
     );
