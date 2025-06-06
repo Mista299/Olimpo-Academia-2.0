84a56d8588bb98af0ecbe7854c871a4492f5c7ad
Mista299 <michael.tabares@udea.edu.co>
2025-06-06 13:51:13 -0500

    new image

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import image1 from "../../imgs/carousel1.webp";
 import image2 from "../../imgs/carousel2.webp";
 import image3 from "../../imgs/carousel3.webp";
+import image4 from "../../imgs/carousel4.jpeg";
 import CarouselItem from "../CarouselItem";
 import logo from "../../imgs/logo2.webp";
 import './styles.css';
 
 const Carousel = () => {
-  const images = [image1, image2, image3];
+  const images = [image1, image2, image3, image4];
 
   // Estado para el índice actual de la imagen
   const [currentIndex, setCurrentIndex] = useState(0);
